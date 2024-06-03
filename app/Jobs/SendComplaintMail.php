1e73aeef30736fc5b17b170aaa5be0661011a512
dafidafi25 <46749869+dafidafi25@users.noreply.github.com>
2024-06-03 22:03:59 +0700

    Update SendComplaintMail.php

--- app/Jobs/SendComplaintMail.php
+++ app/Jobs/SendComplaintMail.php
@@ -7,6 +7,8 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use App\Models\Komplain;
+use Illuminate\Support\Facades\Mail;
 
 class SendComplaintMail implements ShouldQueue
 {
@@ -25,6 +27,29 @@ class SendComplaintMail implements ShouldQueue
      */
     public function handle(): void
     {
-        //
+        $komplains = Komplain::where('created_at', '=', now())
+                            ->where('id_status', '=', 1) // Misalnya Anda ingin filter berdasarkan id_status
+                            ->where('id_level', '=', 1)  // Misalnya Anda ingin filter berdasarkan id_level
+                            ->get();
+        
+        // Jika tidak ada komplain yang memenuhi kriteria, keluar dari fungsi
+        if ($komplains->isEmpty()) {
+            return;
+        }
+    
+        foreach ($komplains as $komplain) {
+            $data_email = [
+                'jenis_pasien' => $komplain->jenis_pasien,
+                'nama' => $komplain->nama,
+                'judul' => $komplain->judul,
+                'kronologi' => $komplain->kronologi,
+            ];
+    
+            // Kirim email dengan data yang disiapkan
+            Mail::to(["ardontallan0904@gmail.com", "ardonyunors147@gmail.com"])->send(new CountdownCSO($data_email));
+    
+            // Set status email_sent menjadi true setelah email dikirim
+            $komplain->update(['email_sent' => true]);
+        }
     }
 }
