15b4c84924f53b5765a82bd06276ef5718525acd
DomicMae <ardonyunors147@gmail.com>
2024-06-06 01:31:41 +0700

    Edit Update.sh

--- update.sh
+++ update.sh
@@ -2,5 +2,6 @@
 
 git pull
 docker build -t web-gr-complaint:latest .
-docker stop gr-complaint-container && docker rm gr-complaint-container
-docker run -d --name gr-complaint-container -p 80:80 web-gr-complaint:latest
+cd ..
+docker compose down
+docker compose up -d
