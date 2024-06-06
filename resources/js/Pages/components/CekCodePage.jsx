6934ddba8aa8f1bbfaa719df99237c5ff20e8023
DomicMae <ardonyunors147@gmail.com>
2024-06-06 20:00:49 +0700

    Edit

--- resources/js/Pages/components/CekCodePage.jsx
+++ resources/js/Pages/components/CekCodePage.jsx
@@ -369,7 +369,10 @@ const CekCodePage = ({ title, description }) => {
                                         className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                                         type="button"
                                         onClick={() => {
-                                            handleCopyToClipboard();
+                                            console.log("Copy button clicked"); // Debugging
+                                            handleCopyToClipboard(
+                                                apiResponse.kode
+                                            ); // Pastikan parameter diteruskan
                                             setShowModal(false);
                                             setKonfirmasiData(false);
                                         }}
--- resources/js/Pages/components/InputDataKomplain.jsx
+++ resources/js/Pages/components/InputDataKomplain.jsx
@@ -854,7 +854,12 @@ const InputDataKomplain = ({ title, description }) => {
                                                             className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                                                             type="button"
                                                             onClick={() => {
-                                                                handleCopyToClipboard();
+                                                                console.log(
+                                                                    "Copy button clicked"
+                                                                ); // Debugging
+                                                                handleCopyToClipboard(
+                                                                    apiResponse.kode
+                                                                ); // Pastikan parameter diteruskan
                                                                 setShowModal(
                                                                     false
                                                                 );
